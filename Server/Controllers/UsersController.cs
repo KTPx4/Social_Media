7f1e6e814415fe862255b4d2d8f87c76fb2a6591
Px4 <px4.vnd@gmail.com>
2025-01-03 16:09:17 +0700

    API: User - register, login, reset pass

--- Server/Controllers/UsersController.cs
+++ Server/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -9,133 +12,190 @@ using Microsoft.EntityFrameworkCore;
 using Server.Data;
 using Server.DTOs.Account;
 using Server.Models.Account;
+using Server.Modules;
 using Server.Services;
 
 namespace Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/user")]
     [ApiController]
-    public class UsersController : ControllerBase
+     public class UsersController : ControllerBase
     {
         private readonly APIDbContext _context;
         private readonly UserService _userService;
         private readonly TokenService _tokenService;
+        private readonly GmailSenderService _gmailSenderService;
+        private readonly string _ClientHost;
+        private readonly string _ClientResetUrl;
 
-        public UsersController(APIDbContext context, UserService userService, TokenService tokenService)
+        public UsersController(APIDbContext context, UserService userService, TokenService tokenService, GmailSenderService gmailSenderService, IConfiguration configuration)
         {
             _context = context;
             _userService = userService;
             _tokenService = tokenService;
+            _gmailSenderService = gmailSenderService;
+
+            var clientSettings = configuration.GetSection("ClientSettings");
+            _ClientHost = clientSettings["HostName"];
+            _ClientResetUrl = clientSettings["ResetUrl"];
+
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<String>> Login([FromBody] UserLogin userLogin)
+        [Consumes("application/json", "multipart/form-data", "application/x-www-form-urlencoded")]
+        public async Task<IActionResult> Login([FromBody] LoginModel userLogin)
         {
             try
             {
-                String token = "";
+                String token = "";                
 
                 var user = await _userService.ValidateLoginAsync(userLogin.UserName, userLogin.Password);
+               
                 if (user == null)
                 {
-                    return Unauthorized("Invalid username or password");
+                    return BadRequest(new {message = "Invalid username or password" });
                 }
-                token =  _tokenService.GenerateToken(userLogin.UserName, "User");
-                return Ok(token);
+
+                token =  _tokenService.GenerateUserToken(user.Id, userLogin.UserName);
+                
+                var rs = new 
+                { 
+                    data = token, 
+                    message = "Login success" 
+                };
+               
+                return Ok(rs);
             }
             catch (Exception ex) 
             {
-                Console.WriteLine(ex.Message);
-                return BadRequest();
+                Console.WriteLine( "ERR UserController - login: "+ ex.Message);
+                return StatusCode(500, new {message = "Server error. Try again!"});
+            }
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel userRegister)
+        {
+            try
+            {
+                
+                var user = await _userService.RegisterAsync(userRegister.UserName, userRegister.Password, userRegister.Email);
+
+                if(user == null)
+                {
+                    return BadRequest(new { message = "Username is exists" });
+                }
+
+                String token = _tokenService.GenerateUserToken(user.Id, user.UserName);
+                var rs = new
+                {
+                    message = "Create account success",
+                    data = user,
+                    token = token
+                };
+                return Ok(rs);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERR UserController - Register: " + ex.Message);
+                return StatusCode(500, new { message = "Server error. Try again!" });
+            }
+        }
+
+        [HttpGet("me")]
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> GetInfo()
+        {
+            try
+            {
+                // Lấy username , id từ claims
+                var userId = User.FindFirstValue("UserId");
+               
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized(new { message = "User not found in token" });
+                }
+
+                return Ok(userId);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERR UserController - GetInfo: " + ex.Message);
+                return StatusCode(500, new { message = "Server error. Try again!" });
+            }
+        }
+
+
+        [HttpPost("reset")]     
+        public async Task<IActionResult> SendResetPass([FromBody] ResetModel resetModel)
+        {
+            try
+            {
+                var user = await _userService.FindByUserName(resetModel.UserName);
+                if(user == null)
+                {
+                    return BadRequest(new { message = "Username not exists" });
+                }
+
+                var newPass = Generator.RandomString(8);
+                var token = _tokenService.GenerateResetToken(user.Id.ToString(), newPass);
+
+               
+                var placeholders = new Dictionary<string, string>
+                {
+                    { "UserName", user.UserName },
+                    { "NewPassword", newPass },
+                    { "ResetLink", $"{_ClientResetUrl}?token={token}" }
+                };
+                string email = user.Email;
+                await _gmailSenderService.SendEmail(email, "Internal - Reset Password", "PasswordResetTemplate.html", placeholders);
+
+                string emailHide = email.ToLower().Split("@gmail.com")[0];
+
+
+                return Ok( new {message = "An email reset password was sent. Please check email register account"});
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERR UserController - TestMail: " + ex.Message);
+                return StatusCode(500, new { message = "Server error. Try again!" });
+            }
+        }
+
+        [HttpGet("reset")]
+        public async Task<IActionResult> ValidReset([FromQuery] string token)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(token)) return BadRequest(new { message = "Token reset password is null" });
+
+                var pricipal = _tokenService.ValidateToken(token); // check token
+                if (pricipal == null) return BadRequest(new { message = "Token invalid or expired" });
+                
+                var user = await _userService.ResetPassword(pricipal); // reset password
+
+                if (user == null) return BadRequest(new { message = "Token data is invalid" });
+                
+                var loginToken = _tokenService.GenerateUserToken(user.Id, user.UserName); // new token login
+
+                return Ok(new {
+                    message ="Reset password success", 
+                    data = user,
+                    token = loginToken
+                });
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERR UserController - TestMail: " + ex.Message);
+                return StatusCode(500, new { message = "Server error. Try again!" });
             }
         }
 
         // GET: api/Users
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<ActionResult<IEnumerable<User>>> GetUser()
-        {
-            return await _context.Users.ToListAsync();
-        }
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(Guid id)
-        {
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(Guid id, User user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Users
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(String UserName, String Password)
-        {
-            User user =await _userService.AddUserAsync(new User()
-            {
-                UserName = UserName,
-                Password = Password
-            });            
-
-            return CreatedAtAction("GetUser" , user.Id);
-        }
-
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(Guid id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool UserExists(Guid id)
-        {
-            return _context.Users.Any(e => e.Id == id);
-        }
     }
 }
