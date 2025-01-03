5ede932570e918db4d2843e26ac9db64644f81d4
Px4 <px4.vnd@gmail.com>
2025-01-03 22:08:01 +0700

    API: Endpoint CRU for User

---
+++ Server/DTOs/Account/UpdateModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Server.DTOs.Account
+{
+    public class UpdateModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string UserProfile { get; set; }
+
+        [AllowNull]
+        public string Bio { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [AllowNull]
+        public string Gender { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email!")]
+        [Required]
+        public string Email { get; set; }
+        [AllowNull]
+        public string Phone { get; set; }
+
+    }
+}
